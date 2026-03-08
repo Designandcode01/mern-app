fdf62a7a6b56bc255c38c8de90324fcdb7089bd5
Muhammad Ijaz <epassionate01@gmail.com>
2026-03-09 00:44:49 +0500

    Commit 3 - Add client/frontend Auth-System

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,73 +1,34 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ProductDetails from "./pages/ProductDetails";
 
-function App() {
-  const [message, setMessage] = useState('')
-
-  useEffect(() => {
-    // Using the proxy, so we don't need full URL
-    axios.get('/api/test')
-      .then(response => {
-        setMessage(response.data.message)
-      })
-      .catch(error => {
-        console.error('Error connecting to backend:', error)
-      })
-  }, [])
-
+function Layout() {
   return (
-    <div className="App">
-      <h1>MERN App</h1>
-      <p>Backend says: {message || 'Loading...'}</p>
-    </div>
-  )
+    <>
+      {/* <Navbar /> */}
+      <Outlet />
+    </>
+  );
 }
+// doe5
+// doe5@example.com
+// Password125
 
-export default App
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/product/:id", element: <ProductDetails /> },
+      
+    ],
+  },
+]);
 
-
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
+export default function App() {
+  return <RouterProvider router={router} />;
+  }
---
+++ client/src/pages/Practice.jsx
@@ -0,0 +1,11 @@
+export default function PracticeAuth() {
+    return (
+        <>
+        <div className="bg-amber-400 min-h-screen flex justify-center items-center p-5">
+
+        <div>Sign Up</div>
+        </div>
+
+        </>
+    )
+}
